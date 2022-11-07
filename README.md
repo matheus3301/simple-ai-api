497dda4a41dde5548444a3c85fe9eba3c8fe2380
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2022-11-07 01:59:51 -0300

    add: working with tf2

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # simple-ai-api
 
+
 ```
+ping raspberrypi.local
+ssh pi@ipdopingai e senha
+
 flask --app api run
 ```
--- api.py
+++ api.py
@@ -1,12 +1,45 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+
+import skimage
+import tensorflow as tf
+import numpy as np
 
 app = Flask(__name__)
 
+interpreter = tf.lite.Interpreter(model_path="./modeling/mnist.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+
 @app.get("/")
 def index():
-    return render_template("index.html")
+    ans = request.args.get('ans')
+    return render_template("index.html", ans=ans)
 
 @app.post("/process")
 def handle_input():
-    pass
+    img_input = request.files['image']
+    img_input.save("./server/tmp/img.jpg")
+
+    input_data = skimage.io.imread("./server/tmp/img.jpg")
+    input_data = input_data.astype(input_details[0]['dtype'])
+    input_data = np.expand_dims(input_data, axis=0)
+
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    interpreter.invoke()
+
+    output = interpreter.get_tensor(output_details[0]['index'])
+
+    out_as_list = output[0].tolist()
+
+    ans = out_as_list.index(max(out_as_list))
+
+    return redirect(url_for('index', ans=ans))
+
